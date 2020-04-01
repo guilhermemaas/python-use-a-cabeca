57d75cc711449d0b567c2cd953d6aed27331dded
Guilherme Maas <guilherme.maas@gmail.com>
2020-04-01 20:15:23 -0300

    20200401pm

---
+++ for.py
@@ -0,0 +1,58 @@
+>>> for i in [1, 2, 3]:
+	print(i)
+
+	
+1
+2
+3
+>>> for i in range(0, 10):
+	print(i)
+
+	
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> for i in 'Palavra':
+	print(i)
+
+	
+P
+a
+l
+a
+v
+r
+a
+>>>
+
+>>> for i in range(0, 10):
+	print('Head First Rocks!')
+
+	
+Head First Rocks!
+Head First Rocks!
+Head First Rocks!
+Head First Rocks!
+Head First Rocks!
+Head First Rocks!
+Head First Rocks!
+Head First Rocks!
+Head First Rocks!
+Head First Rocks!
+
+>>> for num in range(5):
+	print('TESTE')
+
+	
+TESTE
+TESTE
+TESTE
+TESTE
+TESTE
