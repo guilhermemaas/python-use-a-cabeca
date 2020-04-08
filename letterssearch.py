bd10fe65119db45e9b4f4011d145abf5a741cc6f
Guilherme Maas <guilherme.maas@gmail.com>
2020-04-07 22:09:12 -0300

    20200407pm

--- letterssearch.py
+++ letterssearch.py
@@ -5,11 +5,13 @@ def search4vowels(word:str) -> set:
     for vowel in found:
         print(vowel)
         
+        
 def search4vowels2(word:str) -> set:
     '''Return a boolean based on any vowels found.'''
     vowels = set('aeiou')
     found = vowels.intersection(set(word))
     return bool(found)
+
 
 def search4vowels3(word:str) -> set:
     '''Display any vowels found in asked-for word.'''
@@ -17,23 +19,25 @@ def search4vowels3(word:str) -> set:
     found = vowels.intersection(set(word))
     return found
 
+
 def search4vowels4(word:str) -> set:
     '''Display any vowels found in asked-for word.'''
     vowels = set('aeiou')
     return vowels.intersection(set(word))
 
-def search4letters (phrase:str, letters:str) -> set:
-    '''Display any letters found in asked-for word.'''
-    letters = set(letters)
-    return letters.intersection(set(phrase))
+
+def search4letters (phrase:str, letters:str='aeiou') -> set:
+    """Return a set of the 'letters' found in 'phrase'."""
+    return set(letters).intersection(set(phrase))
 
 
-print(help(search4vowels4))
-search4vowels(str(input('Provide a word to search for vowels: ')).lower())
-print(search4vowels2(str(input('Provide a word to search for vowels: ')).lower()))
-print(search4vowels3(str(input('Provide a word to search for vowels: ')).lower()))
-texto = search4vowels4(str(input('Provide a word to search for vowels: ')).lower())
-print(texto)
+#print(help(search4vowels4))
+#search4vowels(str(input('Provide a word to search for vowels: ')).lower())
+#print(search4vowels2(str(input('Provide a word to search for vowels: ')).lower()))
+#print(search4vowels3(str(input('Provide a word to search for vowels: ')).lower()))
+#texto = search4vowels4(str(input('Provide a word to search for vowels: ')).lower())
+#print(texto)
+print(search4letters(str(input('phrase: ').lower()), str(input('letters: ').lower())))
 
 l = list() #Lista
 d = dict() #Dicionario
